0fdc23cef8a7a9b682f687252cefbb16eb42324f
19759025 <PPGorin@sberbank.ru>
2022-05-30 00:07:28 +0300

    правки стилей

--- src/modules/History/components/Game/index.tsx
+++ src/modules/History/components/Game/index.tsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import { fromUnixTime } from 'date-fns';
+import React, { useState } from 'react';
+import { format } from 'date-fns';
 import { IGame } from 'api/history';
 import Team from '../Team';
 import s from './index.module.scss';
 
 const Game = (props: IGame) => {
   const { teams, id, date } = props;
+  const [isHover, setHover] = useState(false);
 
   return (
-    <div className={s.game}>
+    <div
+      className={s.game}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
       <div className={s.teams}>
         {teams.map((team, i) => (
           // eslint-disable-next-line react/no-array-index-key
-          <Team key={`${id}-team-${i}`} {...team} />
+          <Team key={`${id}-team-${i}`} {...team} isHover={isHover} />
         ))}
       </div>
-      <div className={s.date}>{fromUnixTime(date).toString()}</div>
+      <div className={s.date}>{format(date, 'dd.MM.yy')}</div>
     </div>
   );
 };
--- src/modules/History/components/Team/index.tsx
+++ src/modules/History/components/Team/index.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import clsx from 'clsx';
 import { ITeam } from 'api/history';
 import { IPlayer } from 'api/ladder';
 import CrownIcon from 'components/Icons/CrownIcon';
 import Player from '../Player';
 import s from './index.module.scss';
 
-const Team = (props: ITeam) => {
-  const { players, isWinners } = props;
+interface IHistoryTeamProps extends ITeam {
+  isHover?: boolean;
+}
+
+const Team = (props: IHistoryTeamProps) => {
+  const { players, isWinners, isHover } = props;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 
   return (
-    <div className={s.team}>
-      {isWinners && <CrownIcon className={s.icon} />}
-      {players.map((player: IPlayer) => (
-        <Player key={player.id} {...player} />
-      ))}
+    <div className={clsx(s.team, isHover && s.hover)}>
+      <div className={s.players}>
+        {isWinners && <CrownIcon className={s.icon} />}
+        {players.map((player: IPlayer) => (
+          <Player key={player.id} {...player} />
+        ))}
+      </div>
+      {/* {isHover && <div className={s.info}>team mmr: 123</div>} */}
     </div>
   );
 };
